81552c62511d5b195a957bac0fc4b425b6cf5b66
AndreevaEA <125188667+AndreevaEA@users.noreply.github.com>
2023-02-12 14:56:50 +0300

    Add files via upload

---
+++ lesson 12 Igor/js/popup.js
@@ -0,0 +1,16 @@
+let loginButton=document.querySelector('.login-button');
+let loginPopUp=document.querySelector('.login-popup');
+
+console.log(loginButton);
+console.log(loginPopUp);
+
+loginButton.addEventListener('click', showPopUp);
+function showPopUp(evt){
+  console.log(evt);
+  evt.preventDefault();
+
+
+  loginPopUp.classList.toggle('show-popup');
+
+};
+  
