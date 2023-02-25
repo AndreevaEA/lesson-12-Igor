3e58c75d30d2b094de96cf536180c69e67b95632
Андреева Екатерина <kate_cat@mail.ru>
2023-02-25 20:33:49 +0300

    Макет с добавлением страницы о себе.

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,23 @@
 let loginButton=document.querySelector('.login-button');
 let loginPopUp=document.querySelector('.login-popup');
-
-console.log(loginButton);
-console.log(loginPopUp);
+let closeButton=document.querySelector('.popup-close')
 
 loginButton.addEventListener('click', showPopUp);
-function showPopUp(evt){
+closeButton.addEventListener('click', close); 
+
+function showPopUp(evt) {
   console.log(evt);
   evt.preventDefault();
 
-
   loginPopUp.classList.toggle('show-popup');
-
 };
-  
+
+function close() {
+  loginPopUp.classList.remove('show-popup')
+}
+
+loginPopUp.addEventListener('click',(e) => {
+  if(e.target.classList.contains('show-popup')) {
+    close();
+  }
+})
